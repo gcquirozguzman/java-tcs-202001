fc1c783e74e11a912dbaa1a3409e5aaa58acbb45
Shester Developer <gcquirozguzman@gmail.com>
2020-03-19 16:04:25 -0500

    Ordenamiento de imagenes y codigo

---
+++ src/main/java/TipoDatos.java
@@ -0,0 +1,38 @@
+package main.java;
+
+public class TipoDatos {
+
+	public static void main(String[] args) {
+		
+		// Tipo de Datos Enteros (long int short byte)
+		
+		byte valorByte = 127;
+		
+		short valorShort = 32767;
+		
+		int valorInt = 2147483647;
+		
+		long valorLong = 9223372036854775807L;
+		
+		System.out.println("Valor Byte :" +valorByte);
+		
+		System.out.println("Valor Short :" +valorShort);
+		
+		System.out.println("Valor Int :" +valorInt);
+		
+		System.out.println("Valor Long :" +valorLong);
+		
+		// Se crea variable
+		int miPrimerInt;
+		
+		// Se asigna valor
+		miPrimerInt = 2020;
+		
+		// Varias variables en una linea
+		int primeraVariable, segundaVariable, terceraVariable;
+		int cuartaVariable = 12, sextaVariable = 99;
+		
+
+	}
+
+}
