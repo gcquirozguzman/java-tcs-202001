50bd3068441eeb21f7015ea176a8aa4c3b74e154
Gian Carlo Quiroz Guzman <gian.quirozguzman@tcs.com>
2020-03-24 05:35:46 -0500

    Ordenamiento de imagenes y archivos

---
+++ src/main/java/Vehiculo.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class Vehiculo {
+	
+	String color;
+	
+	String marca;
+	
+	String modelo;
+	
+	Integer peso;
+	
+	public String atributo1;
+	
+	protected String atributo2;
+	
+}
+
