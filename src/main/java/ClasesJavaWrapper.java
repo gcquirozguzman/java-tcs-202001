ba19c925296cc5636016b2ac39e5973b7124843f
Gian Carlo Quiroz Guzman <gian.quirozguzman@tcs.com>
2020-03-23 22:21:49 -0500

    Ordenamiento de imagenes y archivos

--- src/main/java/ClasesJavaWrapper.java
+++ src/main/java/ClasesJavaWrapper.java
@@ -4,15 +4,9 @@ public class ClasesJavaWrapper {
 
 	public static void main(String[] args) {
 		
-		// Tipo de Datos Caracteres (char)
-		
-		char valorChar_a_1 = 'a';
-		
-		char valorChar_a_2 = 97;
-		
-		System.out.println("Valor Char - Forma 1 :" +valorChar_a_1);
-		
-		System.out.println("Valor Char - Forma 2 :" +valorChar_a_2);
+		// Clases Wrapper
+		int variable_int = 20;
+		Integer variable_Integer = variable_int;
 		
 	}
 
