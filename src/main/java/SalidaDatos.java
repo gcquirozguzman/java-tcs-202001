9d19d55a9919856835cf9410a7a6bb477725d8dc
Gian Carlo Quiroz Guzman <gian.quirozguzman@tcs.com>
2020-03-24 02:39:54 -0500

    Ordenamiento de imagenes y archivos

---
+++ src/main/java/SalidaDatos.java
@@ -0,0 +1,28 @@
+package main.java;
+
+public class SalidaDatos {
+
+	public static void main(String[] args) {
+		
+		// Salida de Datos
+		
+		System.out.print("Hola Mundo");
+		System.out.print("Hola Mundo");
+		System.out.print("Hola Mundo");
+		System.out.print("Hola Mundo");
+		
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		
+		System.out.print("Mi nombre es Gian \n");
+		System.out.print("Dato 1 \t Dato 2 \t Dato 3 \n");
+		System.out.print("A \t B \t C \n");
+		System.out.print("\\\\ \" Un comentario impreso \" ");
+		
+		System.out.printf("Hola me llamo %s, tengo %d años, mido %10.2f", "Gian", 60, 4.321);
+		
+	}
+	
+}
