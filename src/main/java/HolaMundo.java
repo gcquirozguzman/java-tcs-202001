45db8b5f71399d1534767cbbaa2ed8b6d710c368
Gian Carlo Quiroz Guzman <gian.quirozguzman@tcs.com>
2020-03-24 00:41:15 -0500

    Ordenamiento de imagenes y archivos

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,21 @@
+package main.java;
+
+/**
+ * 
+ * @author Gian Carlo Quiroz
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * Primer método en Java
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Este es un mensaje en la consola de Eclipse
+		System.out.println("Hola Mundo soy Gian");
+	}
+
+}
