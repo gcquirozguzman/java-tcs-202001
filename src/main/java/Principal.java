8e4833fd1091098aed28acf5733b06788950792d
Gian Carlo Quiroz Guzman <gian.quirozguzman@tcs.com>
2020-03-25 02:40:36 -0500

    Ordenamiento de imagenes y archivos

---
+++ src/main/java/Principal.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println(VariablesSistema.ip_base_datos);
+		
+		VariablesSistema variablesSistema = new VariablesSistema();
+		System.out.println(variablesSistema.puerto);
+		
+		//VariablesSistema.dias_semana = 20;
+		
+	}
+	
+}
