053506a05931adcf335e066e85f3ada77bfe78ab
Gian Carlo Quiroz Guzman <gian.quirozguzman@tcs.com>
2020-03-25 05:06:30 -0500

    Ordenamiento de imagenes y archivos

---
+++ src/main/java/EjecutableTCS.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import javax.swing.JOptionPane;
+
+public class EjecutableTCS {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "La prueba es exitosa", "Taller Java", JOptionPane.INFORMATION_MESSAGE);
+		
+	}
+
+}
